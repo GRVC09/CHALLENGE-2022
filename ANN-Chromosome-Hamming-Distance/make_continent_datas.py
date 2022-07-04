8f7f2cbdb14e38823522d9a35ec8c9bf58bd060b
GRVC09 <86490377+GRVC09@users.noreply.github.com>
2022-07-04 18:09:07 -0500

    Update make_continent_datas.py

--- ANN-Chromosome-Hamming-Distance/make_continent_datas.py
+++ ANN-Chromosome-Hamming-Distance/make_continent_datas.py
@@ -1,27 +1,30 @@
+'''
+This algorithm takes the data separated by continent and writes in each line of *_set.dat the information for a single sample.
+'''
 num_per_chrom=[]
 num_per_continet=[]
 num_total=0
-with open('Information.txt','r') as info:			#From the Information file, we get the number of reads per chromosome and the 
-	for i in range(1,5):							#number of known samples per continent
+with open('Information.txt','r') as info:			#Read how many alleles are per chromosome and how many samples are per continent
+	for i in range(1,5):
 		line=info.readline()
 		if i==2:
 			a=line.split()
 		if i==4:
 			b=line.split()
 for i in range(len(a)):
-	num_per_chrom.append(int(a[i]))
+	num_per_chrom.append(int(a[i]))				#Number of alleles per chromosome
 	num_total+=int(a[i])
 print(num_per_chrom)
 for i in range(len(b)):
-	num_per_continet.append(int(b[i]))
+	num_per_continet.append(int(b[i]))			#Number of samples per continent
 print(num_per_continet)
+print('Making groups of continents')
+continents={'EUR','AFR','AMR','SAS','EAS','UNK'}
 
-continents={'EUR','AFR','AMR','SAS','EAS','UNK'}				#List of continents
+sampl_per_continent={'EUR':int(num_per_continet[0]),'AMR':int(num_per_continet[1]),'SAS':int(num_per_continet[2]),	#Dictionary of continent and samples
+'EAS':int(num_per_continet[3]),'AFR':int(num_per_continet[4]),'UNK':int(num_per_continet[5])}
 
-sampl_per_continent={'EUR':int(num_per_continet[0]),'AMR':int(num_per_continet[1]),'SAS':int(num_per_continet[2]),							#Dictionaries between continents and how many samples have,
-'EAS':int(num_per_continet[3]),'AFR':int(num_per_continet[4]),'UNK':int(num_per_continet[5])}												#continents and the *.dat where there are all the information 
-																																			#for each continent, and continents with the *_set.dat file
-continent_dat={'EUR':'EUR.dat','AMR':'AMR.dat','SAS':'SAS.dat', 'EAS':'EAS.dat' ,'AFR':'AFR.dat','UNK':'UNK.dat'}							#where we store only the values that we will need in the next steps
+continent_dat={'EUR':'EUR.dat','AMR':'AMR.dat','SAS':'SAS.dat', 'EAS':'EAS.dat' ,'AFR':'AFR.dat','UNK':'UNK.dat'}	#Dictionaries of read / write files 
 
 continent_set={'EUR':'EUR_set.dat','AMR':'AMR_set.dat','SAS':'SAS_set.dat', 'EAS':'EAS_set.dat' ,'AFR':'AFR_set.dat','UNK':'UNK_set.dat'}
 
@@ -38,9 +41,10 @@ for continent in continents:
 			if 	ref==num_total:
 				count+=1
 				ref=0
-			value=int(a[2])+int(a[3])																										#In this case, we consider that 1|1 is only 1, which that means
-			if value>=1:																													#In this position, the chromosome has the REF and ALT forms in
-				value=1																														#both cases, so we consider only that scenario as one
+			value=int(a[2])+int(a[3])
+			if value>=1:
+				value=1
+			continent_chrom[count].append(value)
 			ref+=1
 
 	fh = open(continent_set[continent],'w')
@@ -51,4 +55,5 @@ for continent in continents:
 			fh.write(str(continent_chrom[i][j]))
 			fh.write('\t')
 		fh.write('\n')
+
 
